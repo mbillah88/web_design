c18afdf5432d074cc62868892fe6ff6b14502e49
mbillah88 <itcmbillah007@gmail.com>
2024-12-25 19:58:53 +0600

    251224

--- servicelinktech/business_apps/static/business_apps/js/new.js
+++ servicelinktech/business_apps/static/business_apps/js/new.js
@@ -16,5 +16,29 @@ function sidebarBtnNav(button) {
      // var element = document.getElementsByClassName("sub-menu");
       //element.classList.toggle("show");    
       document.querySelector(".sub-item").classList.toggle("show");
+      document.querySelector(".drop-btn").classList.toggle("rotate");
    }
               
+   
+function myFunction(x) {
+   if (x.matches) { // If media query matches
+     document.body.style.backgroundColor = "yellow";
+     document.querySelector("#sidebar").classList.add("nav-side-bar");
+     document.querySelector(".side-nav-body").classList.add("colaps");
+   } else {
+     document.body.style.backgroundColor = "pink";
+     document.querySelector(".side-nav-body").classList.remove("colaps");
+     document.querySelector("#sidebar").classList.add("nav-side-bar");
+   }
+ }
+ 
+ // Create a MediaQueryList object
+ var x = window.matchMedia("(max-width: 600px)")
+ 
+ // Call listener function at run time
+ myFunction(x);
+ 
+ // Attach listener function on state changes
+ x.addEventListener("change", function() {
+   myFunction(x);
+ });
