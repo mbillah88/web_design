6ad4b3a7e44a9ea68d9703d76cba6dd0a4702b13
mbillah88 <itcmbillah007@gmail.com>
2024-12-25 18:46:32 +0600

    aa

---
+++ servicelinktech/business_apps/static/business_apps/js/new.js
@@ -0,0 +1,20 @@
+// Sidenav Button  
+function sidebarBtn(button) {
+   // var element = document.getElementsByClassName("sub-menu");
+    //element.classList.toggle("show");    
+    document.querySelector("#sidebar").classList.toggle("nav-side-bar");
+ }
+ // Sidenav Button  
+function sidebarBtnNav(button) {
+   // var element = document.getElementsByClassName("sub-menu");
+    //element.classList.toggle("show");    
+    document.querySelector(".side-nav-body").classList.toggle("colaps");
+ }
+
+   // Sidenav Button  
+   function sidebarBtn1(button) {
+     // var element = document.getElementsByClassName("sub-menu");
+      //element.classList.toggle("show");    
+      document.querySelector(".sub-item").classList.toggle("show");
+   }
+              
